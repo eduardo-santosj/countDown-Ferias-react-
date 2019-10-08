a7802705bffe23932c8d14d0609a740794252411
Bruno Gomes <baraujo@cobasi.com.br>
2019-10-08 14:17:37 -0300

    Up

---
+++ build/precache-manifest.dc89d5b06612b8c33f5688646f78d87f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd92aaec570a5fa8b8aa98c496330afd",
+    "url": "/countDown-Ferias-react-/index.html"
+  },
+  {
+    "revision": "c207154c8150e4af6634",
+    "url": "/countDown-Ferias-react-/static/css/main.b911162f.chunk.css"
+  },
+  {
+    "revision": "c3f231e650888fb48590",
+    "url": "/countDown-Ferias-react-/static/js/2.675a0934.chunk.js"
+  },
+  {
+    "revision": "c207154c8150e4af6634",
+    "url": "/countDown-Ferias-react-/static/js/main.2c2533f6.chunk.js"
+  },
+  {
+    "revision": "3987142c2c3ab3a8dd0f",
+    "url": "/countDown-Ferias-react-/static/js/runtime-main.aa66d3e4.js"
+  },
+  {
+    "revision": "e1caf70964e5c6dc875e1837c4ddc0d9",
+    "url": "/countDown-Ferias-react-/static/media/fogos.e1caf709.gif"
+  },
+  {
+    "revision": "ebe2c937cf58be688cb3f453e3c2c725",
+    "url": "/countDown-Ferias-react-/static/media/tenor.ebe2c937.gif"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d9725a1571f4cd6495007d97e383f22.js"
+  "/countDown-Ferias-react-/precache-manifest.dc89d5b06612b8c33f5688646f78d87f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/countDown-Ferias-react-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
