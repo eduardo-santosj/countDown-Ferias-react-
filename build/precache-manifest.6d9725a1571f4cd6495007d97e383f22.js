cc3333d407ca2b6c94a60044550378d184185ea4
Bruno Gomes <baraujo@cobasi.com.br>
2019-10-08 13:51:06 -0300

    live

---
+++ build/precache-manifest.6d9725a1571f4cd6495007d97e383f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebbacbb387f26bb158b9dccee75566f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b226fa930234aca9828",
+    "url": "/static/css/main.b911162f.chunk.css"
+  },
+  {
+    "revision": "c3f231e650888fb48590",
+    "url": "/static/js/2.675a0934.chunk.js"
+  },
+  {
+    "revision": "8b226fa930234aca9828",
+    "url": "/static/js/main.d64dabe1.chunk.js"
+  },
+  {
+    "revision": "7ba81b9dd10a3c9e8b57",
+    "url": "/static/js/runtime-main.8d5cb912.js"
+  },
+  {
+    "revision": "e1caf70964e5c6dc875e1837c4ddc0d9",
+    "url": "/static/media/fogos.e1caf709.gif"
+  },
+  {
+    "revision": "ebe2c937cf58be688cb3f453e3c2c725",
+    "url": "/static/media/tenor.ebe2c937.gif"
+  }
+]);
