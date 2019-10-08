12ce6d47350e19b4c33b61d964bea5b69c5c9aa9
Bruno Gomes <baraujo@cobasi.com.br>
2019-10-08 13:47:19 -0300

    Contagem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { useEffect , useState } from 'react';
 import './App.css';
+import back from './tenor.gif';
+import fogos from './fogos.gif';
 
-function App() {
-  return (
+const App = () => {
+	const [ time, settime] = useState({
+		Days:0,
+		Hour:0,
+		Min:0,
+		Sec:0
+	});
+	const vacationStart = new Date("2019-10-16 17:00");
+
+	useEffect(() => {
+		counter(vacationStart);
+	}, []);
+
+	
+
+	const counter = (vacationStart) =>{
+		setTimeout( () => {
+		
+			var diffTime = vacationStart - new Date();
+
+			console.log(diffTime);
+
+			if(diffTime < 0){
+				settime({
+					Days:0,
+					Hour:0,
+					Min:0,
+					Sec :0
+				});
+				return false;
+			}
+
+			const diffDays = parseInt( diffTime / (1000 * 60 * 60 * 24) );
+			const difHour =  parseInt( diffTime / (1000 * 60 * 60 ) );
+			const difMin =  parseInt( diffTime / (1000 * 60 ) );
+			const difSec =  parseInt( diffTime / (1000) );
+			
+			settime( {
+				Days:diffDays,
+				Hour:difHour,
+				Min:difMin,
+				Sec:difSec
+			} );
+
+			counter(vacationStart);
+		  }, 1000);
+	}
+
+	return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		{ 
+			time.Hour < 14 ?
+				<h2>Ja estou de ferias na Australia</h2>:
+				"" 
+		}
+		
+		{
+			 time.Sec === 0?
+				<h1>Estou de ferias carai!!!!!</h1>:
+				<React.Fragment>
+					<label>Entro de ferias em  {time.Days } Dias</label>
+					<label>em horas da { time.Hour } horas</label>
+					<label>em minutos da { time.Hour } minutos</label>
+					<label>em segundos da { time.Sec } segundos</label>
+				</React.Fragment>
+		}
+		
+		<img src={ parseInt(time.Sec) === 0 ? fogos : back} />
     </div>
   );
 }
+
+
 
 export default App;
